aaef973683a04c7f64bc73da0fba5e96d5193c2b
Fatima Nguyen <fatima3455@hotmail.com>
2020-08-19 18:38:37 +0000

    eslint on assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -35,14 +35,14 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-  const assertObjectsEqual = function(actual, expected) {
-    const inspect = require('util').inspect; 
-    if (eqObjects(actual, expected)) {
-      console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-    }
-  };
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
