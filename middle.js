7be482c3909a749f856a3c94ecc4fe25f4b17d37
Fatima Nguyen <fatima3455@hotmail.com>
2020-08-19 01:25:23 +0000

    added middle function

---
+++ middle.js
@@ -0,0 +1,49 @@
+const assertArraysEqual = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    }
+  }
+  return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//create middle function
+const middle = arr => {
+  //check if array has more than 2 elements (if not, return empty array)
+  const overTwoElements = arr => arr.length > 2;
+  const isOdd = arr => arr.length % 2 !== 0;
+  const singleMiddle = function(arr) {
+    let index = Math.floor(arr.length / 2);
+    return arr[index];
+  };
+  const doubleMiddle = arr => {
+    let index = arr.length / 2;
+    return [arr[index - 1], arr[index]];
+  };
+  // if array more than 2 elements and odd number --> return single middle elemnt
+  if (overTwoElements(arr) && isOdd(arr)){
+    return singleMiddle(arr);
+  } else if (overTwoElements(arr) && !isOdd(arr)){
+    return doubleMiddle(arr);
+  } else {
+    return [];
+  }
+};
+//array more than 2 elements and even number --> return 2 middle elemens
+
+
+assertArraysEqual(middle([1]), [])
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
