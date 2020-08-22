d6fdde954f1f00c7c353455afab405882a46eabd
Fatima Nguyen <fatima3455@hotmail.com>
2020-08-22 14:29:26 +0000

    eqObjects now works with nested objects using recursion

--- eqArray.js
+++ eqArray.js
@@ -18,8 +18,8 @@ const eqArrays = (arr1, arr2) => {
 module.exports = eqArrays
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
--- eqObjects.js
+++ eqObjects.js
@@ -2,30 +2,39 @@ const assertEqual = require('./assertEqual')
 
 const eqArrays = require('./eqArray')
 
-
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (const key of Object.keys(object1)) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else {
-        if (object1[key] !== object2[key]) {
-          return false;
-        }
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (const key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
       }
     }
-  } else {
-    return false;
+    if (typeof object1[key] === 'object' && typeof object1[key] === 'object') {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
   }
   return true;
 };
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true);
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false);
+
+// eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })
+// assertEqual(eqObjects({ a: { z: 1 }, b: {m: 5} }, { a: { z: 1 }, b: {b: 2} }), false;
+
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
