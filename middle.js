2c7e62833d1d3cbb9ca8f29c77ca01429e0558a4
Fatima Nguyen <fatima3455@hotmail.com>
2020-08-24 11:51:45 +0000

    refactor middle

--- middle.js
+++ middle.js
@@ -22,12 +22,5 @@ const middle = arr => {
     return [];
   }
 };
-//array more than 2 elements and even number --> return 2 middle elemens
 
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
