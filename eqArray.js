68c9874db25bb92f342cd0621580faea6adab49f
Fatima Nguyen <fatima3455@hotmail.com>
2020-08-24 11:14:45 +0000

    refactor eqArrays and assertArrayEqual Test

--- eqArray.js
+++ eqArray.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
@@ -20,12 +18,3 @@ const eqArrays = (arr1, arr2) => {
 };
 
 module.exports = eqArrays;
-
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
